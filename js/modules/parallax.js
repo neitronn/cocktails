27353794a913f4994431dd9f7b6af33230da80bc
igor <neitron_h19@mail.ru>
2023-11-16 06:51:27 +0300

    adding scripts to ignore

--- js/modules/parallax.js
+++
@@ -1,8 +0,0 @@
-export default function ParallaxBanner(){
-    var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent);
-    if(!isMobile) {
-        if($(".parallax").length){  $(".parallax").sitManParallex({  invert: false });}
-    }else{
-        $(".parallax").sitManParallex({  invert: true });
-    }
-}
